70023240dfb06d0f6bccd51641a2e710662ce023
Umka-dev <design.tvin@gmail.com>
2024-07-10 14:58:19 +0200

    install prettier and ajust eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,27 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import parser from '@babel/eslint-parser';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,jsx}'] },
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 2023,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
   pluginReactConfig,
   {
