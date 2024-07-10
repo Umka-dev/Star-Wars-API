0d260cf328b9336a855bce668db757d9cad77e82
Umka-dev <design.tvin@gmail.com>
2024-07-10 15:18:38 +0200

    install new dependencies, change app.jsx code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import { Container, Typography } from '@mui/material';
+import { styled } from '@mui/system';
 
-function App() {
+const StyledTypography = styled(Typography)`
+  && {
+    font-size: 2rem; /* Default size */
+    font-weight: normal;
+    margin-top: 56px;
+
+    @media (min-width: 600px) {
+      font-size: 2.5rem; /* Size for sm screens */
+    }
+
+    @media (min-width: 960px) {
+      font-size: 3rem; /* Size for md screens */
+    }
+
+    @media (min-width: 1280px) {
+      font-size: 3.5rem; /* Size for lg screens */
+    }
+
+    @media (min-width: 1920px) {
+      font-size: 4rem; /* Size for xl screens */
+    }
+  }
+`;
+
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <GlobalStyles
+        styles={{
+          body: {
+            margin: '0',
+            backgroundColor: '#282c34',
+            color: '#fff',
+          },
+        }}
+      />
+      <StyledTypography variant='h1' align='center' gutterBottom>
+        Star Wars Characters
+      </StyledTypography>
+    </Container>
   );
-}
+};
 
 export default App;
