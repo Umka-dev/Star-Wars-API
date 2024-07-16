5195a4efc197f199656a509a4adf2ee984416eea
NikV34 <25172610+NikV34@users.noreply.github.com>
2024-07-16 20:40:55 +0200

    chore: remove Container

--- src/characters/Cards.jsx
+++ src/characters/Cards.jsx
@@ -7,7 +7,7 @@ const Cards = ({ characterList, currentCount, totalCount }) => {
   if (currentCount === 0) return <h4>Characters were not found.</h4>;
 
   return (
-    <Container maxWidth='xl' align='center'>
+    <>
       <Grid container spacing={4} justifyContent='center'>
         {characterList.map(({ id, name, image }) => (
           <Grid key={id} item xs={10} sm={6} md={4} lg={3} xl={2.2}>
@@ -21,7 +21,7 @@ const Cards = ({ characterList, currentCount, totalCount }) => {
       <Typography variant='subtitle2' sx={{ margin: '30px' }}>
         Characters shown {currentCount} from {totalCount}
       </Typography>
-    </Container>
+    </>
   );
 };
 
