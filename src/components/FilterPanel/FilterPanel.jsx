3241c77595abd23ca2034c3629b183360b021926
NikV34 <25172610+NikV34@users.noreply.github.com>
2024-08-07 22:08:10 +0200

    chore: split FilterPanel on components

--- src/components/FilterPanel/FilterPanel.jsx
+++ src/components/FilterPanel/FilterPanel.jsx
@@ -13,57 +13,91 @@ import FilterListIcon from '@mui/icons-material/FilterList';
 import CloseIcon from '@mui/icons-material/Close';
 
 import { useCharactersContext } from '../../context/CharactersContext';
-import { StatusRadioButtons, GenderRadioButtons } from '.';
+import { StatusRadioButtons, GenderRadioButtons } from '../characters';
 
 import { commonStyles } from '../../constants';
 
-const FilterPanel = () => {
-  const { filters, handleNameChange, handleApplyFilters, handleResetFilters } =
-    useCharactersContext();
+const FilterModal = ({ isOpen, handleClose, children }) => {
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={handleClose}
+      fullWidth
+      maxWidth='sm'
+      sx={{
+        '& .MuiPaper-root': {
+          backgroundColor: commonStyles.backgroundColor,
+          color: commonStyles.primaryTextColor,
+        },
+      }}
+    >
+      <DialogTitle>
+        Filters
+        <IconButton
+          aria-label='close'
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: commonStyles.secondaryTextColor,
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>{children}</DialogContent>
+    </Dialog>
+  );
+};
 
-  const [isFilterDialogOpen, setIsFilterDialogOpen] = React.useState(false);
+const FilterNameField = () => {
+  const {
+    filters: { name },
+    handleNameChange,
+  } = useCharactersContext();
 
-  const handleOpenFilterDialog = () => {
-    setIsFilterDialogOpen(true);
-  };
+  return (
+    <TextField
+      label='Input name'
+      variant='standard'
+      size='small'
+      value={name}
+      onChange={handleNameChange}
+      InputProps={{
+        sx: {
+          '&:before': {
+            borderBottomColor: commonStyles.borderColor,
+          },
+          '&:after': {
+            borderBottomColor: commonStyles.linkColor,
+          },
+          input: {
+            color: commonStyles.primaryTextColor,
+          },
+        },
+      }}
+      InputLabelProps={{
+        sx: {
+          color: commonStyles.primaryTextColor,
+        },
+      }}
+      sx={{ minWidth: '150px' }}
+    />
+  );
+};
 
-  const handleCloseFilterDialog = () => {
-    setIsFilterDialogOpen(false);
-  };
+const FilterContent = () => {
+  const { handleApplyFilters, handleResetFilters } = useCharactersContext();
 
-  const renderFilterContent = () => (
+  return (
     <Stack
       direction={{ xs: 'column', sm: 'row' }}
       spacing={{ xs: 2, sm: 2, md: 6, lg: 8, xl: 10 }}
       alignItems='center'
       px={2}
     >
-      <TextField
-        label='Input name'
-        variant='standard'
-        size='small'
-        value={filters.name}
-        onChange={handleNameChange}
-        InputProps={{
-          sx: {
-            '&:before': {
-              borderBottomColor: commonStyles.borderColor,
-            },
-            '&:after': {
-              borderBottomColor: commonStyles.linkColor,
-            },
-            input: {
-              color: commonStyles.primaryTextColor,
-            },
-          },
-        }}
-        InputLabelProps={{
-          sx: {
-            color: commonStyles.primaryTextColor,
-          },
-        }}
-        sx={{ minWidth: '150px' }}
-      />
+      <FilterNameField />
       <StatusRadioButtons />
       <GenderRadioButtons />
       <Stack direction='row' spacing={2}>
@@ -88,6 +122,18 @@ const FilterPanel = () => {
       </Stack>
     </Stack>
   );
+};
+
+const FilterPanel = () => {
+  const [isFilterDialogOpen, setIsFilterDialogOpen] = React.useState(false);
+
+  const handleOpenFilterDialog = () => {
+    setIsFilterDialogOpen(true);
+  };
+
+  const handleCloseFilterDialog = () => {
+    setIsFilterDialogOpen(false);
+  };
 
   return (
     <>
@@ -132,40 +178,17 @@ const FilterPanel = () => {
             backgroundColor: commonStyles.backgroundColor,
           }}
         >
-          {renderFilterContent()}
+          <FilterContent />
         </Box>
       </Stack>
 
       {/* Modal dialog for filters on small screens */}
-      <Dialog
-        open={isFilterDialogOpen}
-        onClose={handleCloseFilterDialog}
-        fullWidth
-        maxWidth='sm'
-        sx={{
-          '& .MuiPaper-root': {
-            backgroundColor: commonStyles.backgroundColor,
-            color: commonStyles.primaryTextColor,
-          },
-        }}
+      <FilterModal
+        isOpen={isFilterDialogOpen}
+        handleClose={handleCloseFilterDialog}
       >
-        <DialogTitle>
-          Filters
-          <IconButton
-            aria-label='close'
-            onClick={handleCloseFilterDialog}
-            sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
-              color: commonStyles.secondaryTextColor,
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-        </DialogTitle>
-        <DialogContent>{renderFilterContent()}</DialogContent>
-      </Dialog>
+        <FilterContent />
+      </FilterModal>
     </>
   );
 };
